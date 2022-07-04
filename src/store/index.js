a4c871c4bb841ee747d2c86230c208993510b5a8
Zanini <everton.zanini@signainfo.com.br>
2022-07-04 12:34:22 -0300

    upload do projeto

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,27 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    tarefas:[
+      {id:0, titulo: "Ir ao mercado", concluido: false },
+      {id:1, titulo: "Comprar pão", concluido: false },
+      {id:2, titulo: "Levar Sufir no banho", concluido: false },
+    ]
   },
   getters: {
   },
   mutations: {
+    adicionaTarefa(state,titulo){
+      if (titulo) {
+        state.tarefas.push({
+          id:new Date().getTime(),
+          titulo,
+          concluido: false,
+        });
+      }
+    },
+    removeTarefa(state,id){
+      state.tarefas = state.tarefas.filter(tarefa=>tarefa.id !== id);
+    }
   },
   actions: {
   },
---
+++ src/views/Tarefas.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <v-col cols="12">
+      <v-text-field
+        v-model="inputTarefa"
+        label="Qual a sua tarefa?"
+        @keyup.enter="handleTarefa"
+        outlined
+        clearable
+      ></v-text-field>
+    </v-col>
+
+    <v-list flat subheader>
+      <v-list-item-group multiple active-class="">
+        <div v-for="(tarefa, index) in $store.state.tarefas" :key="index">
+          <Tarefa :tarefa="tarefa" />
+        </div>
+      </v-list-item-group>
+    </v-list>
+  </div>
+</template>
+
+<script>
+import Tarefa from "../components/Tarefa";
+
+export default {
+  name: "Home",
+  components: {
+    Tarefa,
+  },
+  data() {
+    return {
+      inputTarefa: "",
+    };
+  },
+  methods: {
+    handleTarefa() {
+        this.$store.commit('adicionaTarefa',this.inputTarefa);
+        this.inputTarefa = null;
+      
+    },
+  },
+};
+</script>
